f24993e3b300affb3a7d9d7f0de6fe1611eb25d8
Fabio Heck <fabio98heck@gmail.com>
2025-06-11 04:16:13 -0300

    Atualização geral das paginas

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -1,18 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ProdutoService } from '../services/produto-service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lista',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './lista.component.html'
+  imports: [CommonModule, FormsModule],
+  templateUrl: './lista.component.html',
+  styleUrls: ['./lista.component.css']
 })
 export class ListaComponent {
+  filtro: string = '';
+
   constructor(public produtoService: ProdutoService, private router: Router) {}
 
   detalharProduto(index: number) {
     this.router.navigate(['/produto', index]);
+  }
+
+  get produtosFiltrados() {
+    return this.produtoService.getProdutos().filter(produto =>
+      produto.nome.toLowerCase().includes(this.filtro.toLowerCase()) ||
+      produto.marca.toLowerCase().includes(this.filtro.toLowerCase())
+    );
   }
 }
