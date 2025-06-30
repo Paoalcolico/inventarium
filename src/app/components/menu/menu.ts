4c8bb646b2ca187df2b0b909a431b84abf3a6fa1
Fabio Heck <fabio98heck@gmail.com>
2025-06-30 19:21:03 -0300

    atualização

--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-menu',
   imports: [CommonModule, RouterModule],
+   styleUrls: ['./menu.css'],
   templateUrl: './menu.html',
 })
 export class MenuComponent {
